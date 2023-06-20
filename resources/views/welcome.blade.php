feb89b719bfe60b811c6ecc9144c2767659c799b
widiyantintan <widiyantiintandok25@gmail.com>
2023-06-20 19:40:57 +0700

    MahasiswaB

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,11 +15,10 @@
 
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
-                <a class="btn btn-dark" href="">Home</a>
+                <a class="btn btn-dark" href="{{ route('home') }}">Home</a>
             </div>
         </div>
     </div>
     @vite('resources/js/app.js')
 </body>
 </html>
-
